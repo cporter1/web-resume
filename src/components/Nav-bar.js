7a85d82b8423da9a87b1c07ab16ec22c180d4872
Colin Porter <coln.porter@gmail.com>
2023-01-26 14:01:28 -0800

    added socials to navbar

--- src/components/Nav-bar.js
+++ src/components/Nav-bar.js
@@ -2,11 +2,17 @@ import Link from "./Link"
 
 export default function NavBar() {
 
-    function adaptTitleSize() {
-        if (window.innerWidth < 620) {
-            return 'mobile-title-text'
+    function adaptTextSize() {
+        if (window.innerWidth < 600) {
+            return 'mobile-socials-label'
         }
-        else return 'title-text'
+        else return 'socials-label'
+    }
+    function adaptMobile() {
+        if (window.innerWidth < 600) {
+            return 'mobile-'
+        }
+        else return ''
     }
     
     if (window.innerWidth > 1150) {
@@ -18,9 +24,25 @@ export default function NavBar() {
                         <Link href='/resume'        className='navbar-link-item'>Resume      </Link>
                         <Link href='/contactinfo'   className='navbar-link-item'>Contact Info</Link>
                     </div>
-
-                    <div className="title-container">
-                        <div className="title-text">Colin Porter</div>
+                    <div className="personals-container">
+                        <div className="title-container">
+                            <div className="title-text">Colin Porter</div>
+                        </div>
+                        <div className="socials-container">
+                            <a className="socials-wrapper" href='https://github.com/cporter1' target='_blank'>
+                                <div className="github-icon"/>
+                                <div className="socials-label" style={{display:'flex'}}>
+                                    github.com/cporter1
+                                </div>
+                            </a>
+                            <a className="socials-wrapper" href='https://linkedin.com/in/colin-porter-44a025174'
+                                target='_blank'>
+                                <div className="linkedin-icon"/>
+                                <div className="socials-label" style={{display:'flex'}}>
+                                    linkedin.com/colin-porter
+                                </div>
+                            </a>
+                        </div>
                     </div>
                 </ul>
             </header>
@@ -38,11 +60,27 @@ export default function NavBar() {
                             <Link href='/contactinfo'   className='navbar-drop-item'>Contact Info</Link>
                         </div>
                     </div>
-                    
-                    <div className="title-container">
-                        <div className={adaptTitleSize()}>Colin Porter</div>
-                    </div>
 
+                    <div className="personals-container">
+                        <div className="title-container">
+                            <div className="title-text">Colin Porter</div>
+                        </div>
+                        <div className="socials-container">
+                            <a className="socials-wrapper" href='https://github.com/cporter1' target='_blank'>
+                                <div className={adaptMobile() + "github-icon"}/>
+                                <div className={adaptTextSize()} style={{display:'flex'}}>
+                                    github.com/cporter1
+                                </div>
+                            </a>
+                            <a className="socials-wrapper" href='https://linkedin.com/in/colin-porter-44a025174'
+                                target='_blank'>
+                                <div className={adaptMobile() + "linkedin-icon"}/>
+                                <div className={adaptTextSize()} style={{display:'flex'}}>
+                                    linkedin.com/colin-porter
+                                </div>
+                            </a>
+                        </div>
+                    </div>
                 </ul>
             </header>
         )
