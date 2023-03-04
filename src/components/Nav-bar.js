ea4e5a432ac6991972e0f05c5df2d1941c6a353f
Colin Porter <coln.porter@gmail.com>
2023-03-04 13:56:22 -0800

    resume update

--- src/components/Nav-bar.js
+++ src/components/Nav-bar.js
@@ -2,12 +2,13 @@ import Link from "./Link"
 
 export default function NavBar() {
 
-    function adaptTextSize() {
-        if (window.innerWidth < 600) {
-            return 'mobile-socials-label'
-        }
-        else return 'socials-label'
+    function hide() {
+        if (window.innerWidth < 450)
+            return 'hide-'; 
+        else 
+            return '';
     }
+
     function adaptMobile() {
         if (window.innerWidth < 600) {
             return 'mobile-'
@@ -15,43 +16,16 @@ export default function NavBar() {
         else return ''
     }
     
-    if (window.innerWidth > 1150) {
-        return (
-            <header>
-                <ul className="navbar-container">
+    return (
+        <header>
+            <ul className="navbar-container">
+                { (window.innerWidth > 1150) ?
                     <div className="link-container">
                         <Link href='/'              className='navbar-link-item'>Home        </Link>
                         <Link href='/resume'        className='navbar-link-item'>Resume      </Link>
                         <Link href='/contactinfo'   className='navbar-link-item'>Contact Info</Link>
                     </div>
-                    <div className="personals-container">
-                        <div className="title-container">
-                            <div className="title-text">Colin Porter</div>
-                        </div>
-                        <div className="socials-container">
-                            <a className="socials-wrapper" href='https://github.com/cporter1' target='_blank' rel="noreferrer">
-                                <div className="github-icon"/>
-                                <div className="socials-label" style={{display:'flex'}}>
-                                    github.com/cporter1
-                                </div>
-                            </a>
-                            <a className="socials-wrapper" href='https://linkedin.com/in/colin-porter-44a025174'
-                                target='_blank' rel="noreferrer">
-                                <div className="linkedin-icon"/>
-                                <div className="socials-label" style={{display:'flex'}}>
-                                    linkedin.com/colin-porter
-                                </div>
-                            </a>
-                        </div>
-                    </div>
-                </ul>
-            </header>
-        )
-    }
-    else {
-        return (
-            <header>
-                <ul className="navbar-container">
+                    :
                     <div className="dropdown-container">
                         <div className="dropdown-icon"/>
                         <div className="navbar-dropdown">
@@ -59,31 +33,31 @@ export default function NavBar() {
                             <Link href='/resume'        className='navbar-drop-item'>Resume      </Link>
                             <Link href='/contactinfo'   className='navbar-drop-item'>Contact Info</Link>
                         </div>
-                    </div>
+                    </div>    
+                }
 
-                    <div className="personals-container">
-                        <div className="title-container">
-                            <div className="title-text">Colin Porter</div>
-                        </div>
-                        <div className="socials-container">
-                            <a className="socials-wrapper" href='https://github.com/cporter1' target='_blank'
-                                rel="noreferrer">
-                                <div className={adaptMobile() + "github-icon"}/>
-                                <div className={adaptTextSize()} style={{display:'flex'}}>
-                                    github.com/cporter1
-                                </div>
-                            </a>
-                            <a className="socials-wrapper" href='https://linkedin.com/in/colin-porter-44a025174'
-                                target='_blank' rel="noreferrer">
-                                <div className={adaptMobile() + "linkedin-icon"}/>
-                                <div className={adaptTextSize()} style={{display:'flex'}}>
-                                    linkedin.com/colin-porter
-                                </div>
-                            </a>
-                        </div>
+                <div className="personals-container">
+                    <div className="title-container">
+                        <div className={adaptMobile()+ "title-text"}>Colin Porter</div>
                     </div>
-                </ul>
-            </header>
-        )
-    }
+                    <div className="socials-container">
+                        <a className="socials-wrapper" href='https://github.com/cporter1' target='_blank' rel="noreferrer">
+                            <div className={adaptMobile() + "github-icon"}/>
+                            <div className={hide() + adaptMobile() + "socials-label"} style={{display:'flex'}}>
+                                github.com/cporter1
+                            </div>
+                        </a>
+                        <a className="socials-wrapper" href='https://linkedin.com/in/colin-porter-44a025174'
+                            target='_blank' rel="noreferrer">
+                            <div className={adaptMobile() + "linkedin-icon"}/>
+                            <div className={hide() + adaptMobile() + "socials-label"} style={{display:'flex'}}>
+                                linkedin.com/colin-porter
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </ul>
+        </header>
+    )
+    
 }
